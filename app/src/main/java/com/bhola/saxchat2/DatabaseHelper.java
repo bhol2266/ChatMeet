1f75034d8374f7a883e1a626a52684794a2e5a65
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-03-19 22:47:23 +0545

    dstfsdf

---
+++ app/src/main/java/com/bhola/saxchat2/DatabaseHelper.java
@@ -0,0 +1,286 @@
+package com.bhola.saxchat2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.bhola.saxchat2.Models.Model_Profile;
+import com.google.gson.Gson;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String TAG = "TAGA";
+    String DbName;
+    String DbPath;
+    Context context;
+    String Database_tableNo;
+    Cursor cursor;
+
+    public DatabaseHelper(@Nullable Context mcontext, String name, int version, String Database_tableNo) {
+        super(mcontext, name, null, version);
+        this.context = mcontext;
+        this.DbName = name;
+        this.Database_tableNo = Database_tableNo;
+        DbPath = "/data/data/" + "com.bhola.saxchat2" + "/databases/";
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+        Log.d("TAGA", "oldVersion: " + oldVersion);
+        Log.d("TAGA", "newVersion: " + newVersion);
+
+
+    }
+
+    public void CheckDatabases() {
+        try {
+            String path = DbPath + DbName;
+            SQLiteDatabase.openDatabase(path, null, 0);
+//            db_delete();
+            //Database file is Copied here
+            checkandUpdateLoginTimes_UpdateDatabaseCheck();
+        } catch (Exception e) {
+            this.getReadableDatabase();
+            Log.d(MyApplication.TAG, "CheckDatabases: " + "First Time Copying " + DbName);
+            CopyDatabases();
+        }
+    }
+
+    public void CopyDatabases() {
+
+
+        try {
+            InputStream mInputStream = context.getAssets().open(DbName);
+            String outFilename = DbPath + DbName;
+            OutputStream mOutputstream = new FileOutputStream(outFilename);
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = mInputStream.read(buffer)) > 0) {
+                mOutputstream.write(buffer, 0, length);
+            }
+            mOutputstream.flush();
+            mOutputstream.close();
+            mInputStream.close();
+            //Database file is Copied here
+            checkandUpdateLoginTimes_UpdateDatabaseCheck();
+        } catch (Exception e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void checkandUpdateLoginTimes_UpdateDatabaseCheck() {
+
+        //       Check for Database Update
+
+        Cursor cursor1 = new DatabaseHelper(context, MyApplication.DB_NAME, MyApplication.DB_VERSION, "DB_VERSION").read_DB_VERSION();
+        while (cursor1.moveToNext()) {
+            int DB_VERSION_FROM_DATABASE = cursor1.getInt(1);
+
+            if (DB_VERSION_FROM_DATABASE != MyApplication.DB_VERSION_INSIDE_TABLE) {
+                DatabaseHelper databaseHelper2 = new DatabaseHelper(context, MyApplication.DB_NAME, MyApplication.DB_VERSION, "DB_VERSION");
+                databaseHelper2.db_delete();
+            }
+
+        }
+        cursor1.close();
+
+    }
+
+
+    public void db_delete() {
+
+        File file = new File(DbPath + DbName);
+        if (file.exists()) {
+            file.delete();
+            Log.d("TAGA", "db_delete: " + "Database Deleted " + DbName);
+
+        }
+        CopyDatabases();
+    }
+
+    public void OpenDatabase() {
+        String path = DbPath + DbName;
+        SQLiteDatabase.openDatabase(path, null, 0);
+
+    }
+
+
+    public Cursor readRandomGirls() {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        if (MyApplication.App_updating.equals("inactive") && MyApplication.userLoggedIn && MyApplication.userLoggedIAs.equals("Google")) {
+
+            String query = "SELECT * FROM " + Database_tableNo + " WHERE LENGTH(images) > 50 ORDER BY RANDOM() LIMIT 30";
+            Cursor cursor = db.rawQuery(query, null);
+            return cursor;
+
+
+        } else {
+            String query = "SELECT * FROM " + Database_tableNo + " WHERE LENGTH(images) > 50 AND censored = 1 ORDER BY RANDOM() LIMIT 30";
+            Cursor cursor = db.rawQuery(query, null);
+            return cursor;
+        }
+    }
+
+    public Cursor readSingleGirl(String username) {
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(Database_tableNo, null, "Username=?", new String[]{Utils.encryption(username)}, null, null, null, null);
+
+        return cursor;
+
+    }
+
+    public Cursor readGirls_Country(String countryName) {
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        if (MyApplication.App_updating.equals("inactive") && MyApplication.userLoggedIn && MyApplication.userLoggedIAs.equals("Google")) {
+            if (countryName.equals("All")) {
+                String query = "SELECT * FROM " + Database_tableNo + " WHERE LENGTH(images) > 50 ORDER BY RANDOM() LIMIT 30";
+                Cursor cursor = db.rawQuery(query, null);
+                return cursor;
+            } else {
+                Cursor cursor = db.query(Database_tableNo, null, "country=?", new String[]{countryName}, null, null, "RANDOM()", "40");
+                return cursor;
+            }
+
+        } else {
+
+            if (countryName.equals("All")) {
+                String query = "SELECT * FROM " + Database_tableNo + " WHERE LENGTH(images) > 50 AND censored = 1 ORDER BY RANDOM() LIMIT 30";
+                Cursor cursor = db.rawQuery(query, null);
+                return cursor;
+
+            } else {
+
+                String selection = "country=? AND censored=?"; // Modify the condition as needed
+                String[] selectionArgs = new String[]{countryName, String.valueOf(1)}; // Replace with your arguments
+
+                Cursor cursor = db.query(Database_tableNo, null, selection, selectionArgs, null, null, null, null);
+                return cursor;
+            }
+        }
+
+
+    }
+
+
+    public String updateCensored(String username, int censoredValue) {
+        SQLiteDatabase sQLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("censored", censoredValue);
+
+        float res = sQLiteDatabase.update(Database_tableNo, contentValues, "Username = ?", new String[]{Utils.encryption(username)});
+        Log.d("sdafdsaf", "updateCensored: " + DbName);
+        Log.d("sdafdsaf", "updateCensored: " + res);
+        Log.d("sdafdsaf", "updateCensored: " + Utils.encryption(username));
+        if (res == -1)
+            return "Failed";
+        else
+            return "Success";
+    }
+
+    public String selectedBot(String username, int selectedBot_Value) {
+        SQLiteDatabase sQLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("selectedBot", selectedBot_Value);
+
+        float res = sQLiteDatabase.update(Database_tableNo, contentValues, "Username = ?", new String[]{Utils.encryption(username)});
+        if (res == -1)
+            return "Failed";
+        else
+            return "Success";
+    }
+
+    public String updateLike(String username, int likeValue) {
+        SQLiteDatabase sQLiteDatabase = getWritableDatabase();
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("like", likeValue);
+
+        float res = sQLiteDatabase.update(Database_tableNo, contentValues, "Username = ?", new String[]{Utils.encryption(username)});
+        if (res == -1)
+            return "Failed";
+        else
+            return "Success";
+    }
+
+    public String addProfiles(Model_Profile model_profile) {
+
+
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("Username", Utils.encryption(model_profile.getUsername()));
+        values.put("Name", Utils.encryption(model_profile.getName()));
+        values.put("Country", model_profile.getFrom());
+        values.put("Languages", model_profile.getLanguages());
+        values.put("Age", model_profile.getAge());
+        values.put("InterestedIn", model_profile.getInterested());
+        values.put("BodyType", model_profile.getBodyType());
+        values.put("Specifics", Utils.encryption(model_profile.getSpecifics()));
+        values.put("Ethnicity", model_profile.getEthnicity());
+        values.put("Hair", model_profile.getHair());
+        values.put("EyeColor", model_profile.getEyeColor());
+        values.put("Subculture", model_profile.getSubculture());
+        values.put("profilePhoto", Utils.encryption(model_profile.getProfilePhoto()));
+        values.put("coverPhoto", Utils.encryption(model_profile.getCoverPhoto()));
+
+        Gson gson = new Gson();
+        String interestsJson = gson.toJson(model_profile.getInterested());
+        values.put("Interests", Utils.encryption(interestsJson));
+
+        String imagesJson = gson.toJson(model_profile.getImages());
+        values.put("images", Utils.encryption(imagesJson));
+
+        String videos = gson.toJson(model_profile.getVideos());
+        values.put("videos", Utils.encryption(videos));
+
+
+        float res = db.insert("GirlsProfile", null, values);
+
+        if (res == -1)
+
+            return "Failed";
+        else
+            return "Sucess";
+
+    }
+
+
+    public void deleteAllrows() {
+        Log.d(TAG, "deleteAllrows: " + Database_tableNo);
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete(Database_tableNo, null, null);
+    }
+
+    public Cursor read_DB_VERSION() {
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor cursor = db.query(Database_tableNo, null, null, null, null, null, null, null);
+        return cursor;
+
+    }
+
+    @Override
+    public void onOpen(SQLiteDatabase db) {
+        super.onOpen(db);
+        db.disableWriteAheadLogging();
+    }
+
+
+}
---
+++ app/src/main/java/com/bhola/saxchat2/MainActivity.java
@@ -0,0 +1,416 @@
+package com.bhola.saxchat2;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.InsetDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.bhola.saxchat2.Models.ChatItem_ModelClass;
+import com.bhola.saxchat2.Models.Model_Profile;
+import com.bhola.saxchat2.Models.UserBotMsg;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static TextView badge_text;
+    public static int unreadMessage_count;
+    public static ViewPager2 viewPager2;
+    com.facebook.ads.InterstitialAd facebook_IntertitialAds;
+    private InAppUpdate inAppUpdate;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+
+//        fullscreenMode();
+        checkForupdate();
+        try {
+            // this is check if user data is available or not , if not start splashscreen
+            String userId = String.valueOf(MyApplication.userModel.getUserId());
+        } catch (Exception e) {
+            startActivity(new Intent(MainActivity.this, SplashScreen.class));
+            return;
+        }
+
+
+        if (MyApplication.Ads_State.equals("active")) {
+            showAds();
+        }
+
+
+        initializeBottonFragments();
+        startIncomingCallService();
+
+
+    }
+
+    private void startIncomingCallService() {
+        if (MyApplication.App_updating.equals("active")) {
+            return;
+        }
+//        Intent intent = new Intent(MainActivity.this, IncomingCallService.class);
+//        startService(intent);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                showFragment();
+            }
+        }, 20000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showFragment();
+            }
+        }, 90000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                showFragment();
+            }
+        }, 210000);
+
+    }
+
+    private void showFragment() {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        if (!fragmentManager.isDestroyed()) {
+            try {
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                Fragment_Calling fragment = new Fragment_Calling();
+
+                Model_Profile randomgirl = Utils.getSingleRandomGirlVideoObject(MainActivity.this);
+
+                Bundle args = new Bundle();
+                args.putString("name", randomgirl.getUsername());
+                args.putString("profile", randomgirl.getProfilePhoto());
+                args.putString("username", randomgirl.getUsername());
+                fragment.setArguments(args);
+                fragmentTransaction.replace(R.id.fragment_container, fragment);
+                fragmentTransaction.commit();
+            } catch (Exception e) {
+
+            }
+        }
+
+
+    }
+
+
+    private void initializeBottonFragments() {
+        viewPager2 = findViewById(R.id.viewpager);
+        viewPager2.setAdapter(new PagerAdapter(MainActivity.this));
+
+        viewPager2.setOffscreenPageLimit(5);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+
+                    case 0:
+                        tab.setIcon(R.drawable.trending);
+                        View view5 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view5.findViewById(R.id.icon).setBackgroundResource(R.drawable.trending);
+                        tab.setCustomView(view5);
+
+                        //By default tab 0 will be selected to change the tint of that tab
+                        View tabView = tab.getCustomView();
+                        ImageView tabIcon = tabView.findViewById(R.id.icon);
+                        tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.themeColor));
+
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.videocall2);
+
+                        View view1 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.videocall2);
+                        tab.setCustomView(view1);
+
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.chat);
+
+
+                        View view2 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.chat);
+                        tab.setCustomView(view2);
+                        unreadMessage_count = getUndreadMessage_Count();
+
+                        badge_text = view2.findViewById(R.id.badge_text);
+                        badge_text.setVisibility(View.GONE);
+
+                        if (!Fragment_Messenger.retreive_sharedPreferences(MainActivity.this)) {
+                            //logged in first time
+                            new Handler().postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    //First time
+                                    badge_text.setVisibility(View.VISIBLE);
+                                    badge_text.setText("1");
+                                    badge_text.setBackgroundResource(R.drawable.badge_background);
+//                                    MediaPlayer mediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.message_received);
+//                                    mediaPlayer.start();
+
+                                }
+                            }, 20000);
+
+                        } else {
+                            if (unreadMessage_count != 0) {
+                                badge_text.setVisibility(View.VISIBLE);
+                                badge_text.setText(String.valueOf(unreadMessage_count));
+                                badge_text.setBackgroundResource(R.drawable.badge_background);
+
+                            } else {
+                                badge_text.setVisibility(View.GONE);
+                            }
+                        }
+
+                        break;
+
+
+                    case 3:
+                        tab.setIcon(R.drawable.info_2);
+
+
+                        View view3 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.info_2);
+                        tab.setCustomView(view3);
+                        break;
+
+
+                    default:
+                        tab.setIcon(R.drawable.user2);
+                        View view4 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view4.findViewById(R.id.icon).setBackgroundResource(R.drawable.user2);
+                        tab.setCustomView(view4);
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Get the custom view of the selected tab
+                View tabView = tab.getCustomView();
+                if (tabView != null) {
+                    // Find the ImageView in the custom view
+                    ImageView tabIcon = tabView.findViewById(R.id.icon);
+
+                    // Set the background tint color for the selected tab
+                    tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, R.color.themeColor));
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Get the custom view of the unselected tab
+                View tabView = tab.getCustomView();
+                if (tabView != null) {
+                    // Find the ImageView in the custom view
+                    ImageView tabIcon = tabView.findViewById(R.id.icon);
+
+                    // Set the background tint color for the unselected tab
+                    tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(MainActivity.this, com.google.android.ads.mediationtestsuite.R.color.gmts_light_gray));
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Tab reselected, no action needed
+            }
+        });
+    }
+
+    private int getUndreadMessage_Count() {
+
+        ArrayList<ChatItem_ModelClass> userListTemp = new ArrayList<>();
+        SharedPreferences sharedPreferences = MainActivity.this.getSharedPreferences("messenger_chats", Context.MODE_PRIVATE);
+
+// Retrieve the JSON string from SharedPreferences
+        String json = "";
+        if (MyApplication.userLoggedIn && MyApplication.userLoggedIAs.equals("Google")) {
+            json = sharedPreferences.getString("userListTemp_Google", null);
+        } else {
+            json = sharedPreferences.getString("userListTemp_Guest", null);
+        }
+
+// Convert the JSON string back to ArrayList
+        Gson gson = new Gson();
+        Type type = new TypeToken<ArrayList<ChatItem_ModelClass>>() {
+        }.getType();
+
+
+        if (json == null) {
+            // Handle case when no ArrayList is saved in SharedPreferences
+            return 0;
+        } else {
+            userListTemp = gson.fromJson(json, type);
+
+            int count = 0;
+            for (int i = 0; i < userListTemp.size(); i++) {
+
+                ChatItem_ModelClass modelclass = userListTemp.get(i);
+
+                for (int j = 0; j < modelclass.getUserBotMsg().size(); j++) {
+                    UserBotMsg userBotMsg = modelclass.getUserBotMsg().get(j);
+                    if (userBotMsg.getSent() == 1 && userBotMsg.getRead() == 0) {
+                        count = count + 1;
+                    }
+                }
+                if (modelclass.isContainsQuestion()) {
+                    if (modelclass.getQuestionWithAns().getSent() == 1 && modelclass.getQuestionWithAns().getRead() == 0) {
+                        count = count + 1;
+                    }
+                }
+            }
+            return count;
+        }
+
+    }
+
+
+    @Override
+    public void onBackPressed() {
+        exit_dialog();
+        if (MyApplication.Ads_State.equals("active")) {
+            showAds();
+        }
+    }
+
+    private void checkForupdate() {
+        inAppUpdate = new InAppUpdate(MainActivity.this);
+        inAppUpdate.checkForAppUpdate();
+
+    }
+
+    private void exit_dialog() {
+
+        final androidx.appcompat.app.AlertDialog.Builder builder = new androidx.appcompat.app.AlertDialog.Builder(MainActivity.this);
+        LayoutInflater inflater = LayoutInflater.from(getApplicationContext());
+        View promptView = inflater.inflate(R.layout.dialog_exit_app, null);
+        builder.setView(promptView);
+        builder.setCancelable(true);
+
+        TextView exit = promptView.findViewById(R.id.confirm);
+        TextView cancel = promptView.findViewById(R.id.cancel);
+
+
+        AlertDialog exitDialog = builder.create();
+        exitDialog.show();
+
+
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (MyApplication.exit_Refer_appNavigation.equals("active") && MyApplication.Login_Times < 2 && MyApplication.Refer_App_url2.length() > 10) {
+
+                    Intent j = new Intent(Intent.ACTION_VIEW);
+                    j.setData(Uri.parse(MyApplication.Refer_App_url2));
+                    try {
+                        startActivity(j);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    finishAffinity();
+                    System.exit(0);
+                    finish();
+                    exitDialog.dismiss();
+
+                } else {
+
+                    finishAffinity();
+                    finish();
+                    System.exit(0);
+                    finish();
+                    exitDialog.dismiss();
+
+                }
+            }
+        });
+
+        cancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                exitDialog.cancel();
+            }
+        });
+
+
+        ColorDrawable back = new ColorDrawable(Color.TRANSPARENT);
+        InsetDrawable inset = new InsetDrawable(back, 20);
+        exitDialog.getWindow().setBackgroundDrawable(inset);
+
+    }
+
+
+    private void showAds() {
+        if (MyApplication.Ad_Network_Name.equals("admob")) {
+            if (!SplashScreen.homepageAdShown) {
+                ADS_ADMOB.Interstitial_Ad(this);
+                SplashScreen.homepageAdShown = true;
+            }
+        } else {
+            if (!SplashScreen.homepageAdShown) {
+                ADS_FACEBOOK.interstitialAd(this, facebook_IntertitialAds, getString(R.string.Facebook_InterstitialAdUnit));
+                SplashScreen.homepageAdShown = true;
+            }
+        }
+    }
+
+    private void fullscreenMode() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+        WindowInsetsControllerCompat windowInsetsCompat = new WindowInsetsControllerCompat(getWindow(), getWindow().getDecorView());
+        windowInsetsCompat.hide(WindowInsetsCompat.Type.statusBars());
+        windowInsetsCompat.setSystemBarsBehavior(WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
+        }
+    }
+
+
+}
