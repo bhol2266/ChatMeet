d278ad741c032dde2a5f8449def41fc9fef380f4
ukdevelopers007 <113181134+ukdevelopers007@users.noreply.github.com>
2024-03-22 21:44:37 +0545

    dsafasdf

--- app/src/main/java/com/bhola/saxchat2/PremiumMembership.java
+++ app/src/main/java/com/bhola/saxchat2/PremiumMembership.java
@@ -1,16 +1,5 @@
 package com.bhola.saxchat2;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.cardview.widget.CardView;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager.widget.ViewPager;
-
 import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
@@ -23,7 +12,20 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.ViewFlipper;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.ViewPager2;
+
 import com.bhola.saxchat2.Models.FlipItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -37,7 +39,7 @@ public class PremiumMembership extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_premium_membership);
-        viewFlipper=findViewById(R.id.viewFlipper);
+        viewFlipper = findViewById(R.id.viewFlipper);
         bottomRecyclerView = findViewById(R.id.bottomRecyclerView);
 
         List<FlipItem> flipItems = new ArrayList<>();
@@ -77,6 +79,108 @@ public class PremiumMembership extends AppCompatActivity {
 //            public void onPageScrollStateChanged(int state) {}
 //        });
 //        fullscreenMode();
+
+        tabBtns();
+    }
+
+    private void tabBtns() {
+
+        ViewPager2 viewPager2 = findViewById(R.id.viewpager);
+        viewPager2.setAdapter(new PagerAdapter(PremiumMembership.this));
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+
+
+        TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position) {
+
+                    case 0:
+                        tab.setIcon(R.drawable.mm_chat);
+                        View view5 = getLayoutInflater().inflate(R.layout.item_flip, null);
+                        TextView textView = view5.findViewById(R.id.text);
+                        textView.setText("Unlimited messages");
+                        tab.setCustomView(view5);
+
+                        //By default tab 0 will be selected to change the tint of that tab
+//                        View tabView = tab.getCustomView();
+//                        ImageView tabIcon = tabView.findViewById(R.id.icon);
+//                        tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(PremiumMembership.this, R.color.themeColor));
+
+                        break;
+                    case 1:
+                        tab.setIcon(R.drawable.videocall2);
+
+                        View view1 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view1.findViewById(R.id.icon).setBackgroundResource(R.drawable.videocall2);
+                        tab.setCustomView(view1);
+
+                        break;
+                    case 2:
+                        tab.setIcon(R.drawable.chat);
+
+
+                        View view2 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view2.findViewById(R.id.icon).setBackgroundResource(R.drawable.chat);
+                        tab.setCustomView(view2);
+
+                        break;
+
+
+                    case 3:
+                        tab.setIcon(R.drawable.info_2);
+
+
+                        View view3 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view3.findViewById(R.id.icon).setBackgroundResource(R.drawable.info_2);
+                        tab.setCustomView(view3);
+                        break;
+
+
+                    default:
+                        tab.setIcon(R.drawable.user2);
+                        View view4 = getLayoutInflater().inflate(R.layout.customtab, null);
+                        view4.findViewById(R.id.icon).setBackgroundResource(R.drawable.user2);
+                        tab.setCustomView(view4);
+                        break;
+                }
+            }
+        });
+        tabLayoutMediator.attach();
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                // Get the custom view of the selected tab
+                View tabView = tab.getCustomView();
+                if (tabView != null) {
+                    // Find the ImageView in the custom view
+                    ImageView tabIcon = tabView.findViewById(R.id.icon);
+
+                    // Set the background tint color for the selected tab
+                    tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(PremiumMembership.this, R.color.themeColor));
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                // Get the custom view of the unselected tab
+                View tabView = tab.getCustomView();
+                if (tabView != null) {
+                    // Find the ImageView in the custom view
+                    ImageView tabIcon = tabView.findViewById(R.id.icon);
+
+                    // Set the background tint color for the unselected tab
+                    tabIcon.setBackgroundTintList(ContextCompat.getColorStateList(PremiumMembership.this, com.google.android.ads.mediationtestsuite.R.color.gmts_light_gray));
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                // Tab reselected, no action needed
+            }
+        });
     }
 
     private void fullscreenMode() {
@@ -111,7 +215,7 @@ class FlipItemAdapter extends RecyclerView.Adapter<FlipItemAdapter.FlipItemViewH
 
     @Override
     public void onBindViewHolder(@NonNull FlipItemViewHolder holder, int position) {
-        FlipItem flipItem=items.get(position);
+        FlipItem flipItem = items.get(position);
 
         holder.text.setText(flipItem.getItemName());
         holder.image.setImageDrawable(flipItem.getItemImage());
